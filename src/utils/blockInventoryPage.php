08dc428eeda8c9b183023cf9ee45a43a06df03ed
Mario Reyes <mager19@Marios-MacBook-Pro.local>
2024-04-25 15:05:55 -0500

    fix url icon

--- src/utils/blockInventoryPage.php
+++ src/utils/blockInventoryPage.php
@@ -9,7 +9,7 @@ class BlockInventoryPage
     public function __construct()
     {
         add_action('admin_menu', array($this, 'add_plugin_page'));
-        $this->icon_url = plugin_dir_url( __FILE__ ) . '../images/40q-wp-icon.svg';
+        $this->icon_url = plugin_dir_url( __DIR__  ) . 'images/40q-wp-icon.svg';
     }
 
     public function add_plugin_page()
