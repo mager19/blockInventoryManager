9e1dbc127de0db7106ee70c3b7804a31dd80cc59
Mario Reyes <mager19@Marios-MacBook-Pro.local>
2024-04-25 15:58:31 -0500

    change version

--- blockinventory.php
+++ blockinventory.php
@@ -4,7 +4,7 @@
  * Description:       A plugin of custom blocks by 40Q.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.1
+ * Version:           0.1.3
  * Author:            40Q
  * Author URI:        https://40Q.agency
  * License:           GPL-2.0-or-later
